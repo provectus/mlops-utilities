99b636da44a1f20c7da53a9c8e8759f90ec24e7e
Aleksandr <abelov@provectus.com>
2022-07-26 17:42:13 +0100

    g

--- tests/pipeline.py
+++ tests/pipeline.py
@@ -2,7 +2,17 @@ from sagemaker import Session
 from omegaconf.dictconfig import DictConfig
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.workflow.steps import ProcessingStep
+from sagemaker.workflow.pipeline_context import PipelineSession
 
+from sagemaker.processing import Processor
+
+pipeline_session = PipelineSession()
+my_processor = Processor(
+    role='arn:aws:iam::311638508164:role/AmazonSageMaker-ExecutionRole',
+    instance_type='ml.m5.xlarge',
+    instance_count=1,
+    sagemaker_session=pipeline_session
+)
 
 def get_pipeline(
         sm_session: Session,
@@ -15,7 +25,11 @@ def get_pipeline(
         steps=[
             ProcessingStep(
                 name=conf.step.name,
-                step_args={'k': 'v'}
+                processor=my_processor,
+                step_args=my_processor.run(
+                    inputs=[],
+                    ouputs=[]
+                )
             )
         ],
         sagemaker_session=sm_session
