c421216a3fdfa7bdb8e2ec76a83979e925b925a1
Aleksandr <abelov@provectus.com>
2022-07-27 17:02:56 +0100

    s

--- tests/pipeline.py
+++ tests/pipeline.py
@@ -31,7 +31,7 @@ def get_pipeline(
                 processor=my_processor,
                 step_args=my_processor.run(
                     inputs=[],
-                    ouputs=[]
+                    outputs=[]
                 )
             )
         ],
