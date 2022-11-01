99472c34ed1fef2f538e70aa5ad76a45a5c7c491
somal <Somal1996!github>
2022-11-01 15:15:04 +0100

    Reformat actions and helpers

--- mlops_utilities/helpers.py
+++ mlops_utilities/helpers.py
@@ -1,24 +1,78 @@
 import json
+import logging
 import operator
+from datetime import datetime
 from functools import reduce
+from pathlib import Path
+from typing import Dict, List, Any, Mapping
 
 import boto3
-import logging
-from pathlib import Path
-from omegaconf import OmegaConf
 from botocore.client import BaseClient
-from typing import Dict, List
+from omegaconf import OmegaConf, dictconfig
 
 
-def get_datetime_str(arg_dt):
-    return arg_dt.strftime('%Y-%m-%d-%H-%M-%S')
+# TODO: check and setup output type for all SM dependent method
+### Sagemaker dependent methods
+
+def get_pipeline_config(pipeline_module, config_type: str, pipeline_role: str, args: List) -> dictconfig:
+    """
+    Read pipeline config
+    :param pipeline_module: python module which includes pipeline
+    :param config_type: config filename
+    :param pipeline_role: IAM role
+    :param args: runtime pipeline arguments
+    :return: OmegaConf Dict Config
+    """
+    default_conf_path = f'{Path(pipeline_module.__file__).parent}/{config_type}.yml'
+    default_conf = OmegaConf.load(default_conf_path)
+    arg_conf = OmegaConf.create({'pipeline': {'role': pipeline_role}})
+    override_arg_conf = OmegaConf.from_dotlist(args)
+    return OmegaConf.merge(default_conf, arg_conf, override_arg_conf)
 
 
-def param_dict_to_nv_list(arg_dict):
-    return [
-        {'Name': k, 'Value': v}
-        for k, v in arg_dict.items()
-    ]
+def get_output_destination(sagemaker_client: BaseClient, processing_job_arn: str, output_name: str):
+    pj_output_dict = _list_to_dict(
+        sagemaker_client.describe_processing_job(
+            ProcessingJobName=get_job_name(processing_job_arn)
+        )['ProcessingOutputConfig']['Outputs'],
+        'OutputName'
+    )
+    return pj_output_dict[output_name]['S3Output']['S3Uri']
+
+
+def get_model_location(sagemaker_client: BaseClient, model_name: str):
+    model_desc = sagemaker_client.describe_model(ModelName=model_name)
+    return model_desc['PrimaryContainer']['ModelDataUrl']
+
+
+def get_approved_package(sagemaker_client: BaseClient, model_package_group_name: str):
+    response = sagemaker_client.list_model_packages(
+        ModelApprovalStatus='Approved',
+        ModelPackageGroupName=model_package_group_name,
+        SortBy='CreationTime',
+        SortOrder='Descending',
+        MaxResults=1,
+    )
+
+    model_packages = response['ModelPackageSummaryList']
+    if len(model_packages) == 0:
+        raise ValueError(f"No approved ModelPackage found for ModelPackageGroup: {model_package_group_name}")
+
+    return model_packages[0]
+
+
+def load_json_from_s3(s3_uri: str) -> Dict:
+    s3_client = boto3.client('s3')
+    bucket, key = s3_uri.replace("s3://", "").split("/", 1)
+    s3_response_object = s3_client.get_object(Bucket=bucket, Key=key)
+
+    return json.loads(s3_response_object['Body'].read().decode('utf-8'))
+
+
+### Common methods
+
+def get_datetime_str(date_time: datetime) -> str:
+    return date_time.strftime('%Y-%m-%d-%H-%M-%S')
 
 
 def convert_param_dict_to_key_value_list(arg_dict: Dict[str, str]) -> List[Dict[str, str]]:
@@ -34,95 +88,44 @@ def convert_param_dict_to_key_value_list(arg_dict: Dict[str, str]) -> List[Dict[
     ]
 
 
-def getValueFromDict(dataDict, mapList):
+def get_value_from_dict(data_dict: Dict[str, Any], path: List[str]) -> Mapping:
     """
        Get necessary metric from evaluation.json
-       :param dataDict: dictionary that contains metrics {{"regression_metrics": {"mse":..}}
-       :param mapList: path to metric ['regression_metrics','mse',…]
+       :param data_dict: dictionary that contains metrics {{"regression_metrics": {"mse":..}}
+       :param path: path to metric ['regression_metrics','mse',…]
        :return: dictionary that contains target metric:
        for example {mse: 2.1} --from--> { "Key 01": { "Key 02:…{mse: 2.1}"}
     """
-    return reduce(operator.getitem, mapList, dataDict)
+    return reduce(operator.getitem, path, data_dict)
 
 
-def normalize_key(key: str):
+def normalize_key(key: str) -> str:
     return key.replace('_', '').lower()
 
 
-def get_model_name(model_arn):
+def get_model_name(model_arn: str) -> str:
     return model_arn[model_arn.rindex('/') + 1:]
 
 
-def get_job_name(job_arn):
+def get_job_name(job_arn: str) -> str:
     return job_arn[job_arn.rindex('/') + 1:]
 
 
-def get_output_destination(sm, processing_job_arn, output_name):
-    pj_output_dict = _list_to_dict(
-        sm.describe_processing_job(
-            ProcessingJobName=get_job_name(processing_job_arn)
-        )['ProcessingOutputConfig']['Outputs'],
-        'OutputName'
-    )
-    return pj_output_dict[output_name]['S3Output']['S3Uri']
-
-
-def get_model_location(sm, model_name):
-    model_desc = sm.describe_model(ModelName=model_name)
-    return model_desc['PrimaryContainer']['ModelDataUrl']
-
-
-def _list_to_dict(arg_list, dict_key_attr):
+def _list_to_dict(arg_list: List[Dict[str, Any]], dict_key_attr: str) -> Dict[Any, Dict]:
     return {
         o[dict_key_attr]: o
         for o in arg_list
     }
 
 
-def ensure_min_length(arg_str, min_length):
-    if len(arg_str) < min_length:
-        return arg_str + '_' + ('0' * (min_length - len(arg_str)))
+def ensure_min_length(argument: str, min_length: int) -> str:
+    if len(argument) < min_length:
+        return f"{argument}_ {'0' * (min_length - len(argument))}"
     else:
-        return arg_str
+        return argument
 
 
-def get_approved_package(sm: BaseClient, model_package_group_name: str):
-    response = sm.list_model_packages(
-        ModelApprovalStatus='Approved',
-        ModelPackageGroupName=model_package_group_name,
-        SortBy='CreationTime',
-        SortOrder='Descending',
-        MaxResults=1,
-    )
-
-    model_packages = response['ModelPackageSummaryList']
-    if len(model_packages) == 0:
-        error_message = (
-            f"No approved ModelPackage found for ModelPackageGroup: {model_package_group_name}"
-        )
-        raise Exception(error_message)
-
-    return model_packages[0]
-
-
-def load_json_from_s3(s3_uri: str):
-    s3_client = boto3.client('s3')
-    bucket, key = s3_uri.replace("s3://", "").split("/", 1)
-    s3_response_object = s3_client.get_object(Bucket=bucket, Key=key)
-
-    return json.loads(s3_response_object['Body'].read().decode('utf-8'))
-
-
-def get_pipeline_config(mod_pipe, config_type, pipeline_role, args):
-    default_conf_path = f'{Path(mod_pipe.__file__).parent}/{config_type}.yml'
-    default_conf = OmegaConf.load(default_conf_path)
-    arg_conf = OmegaConf.create({'pipeline': {'role': pipeline_role}})
-    override_arg_conf = OmegaConf.from_dotlist(args)
-    return OmegaConf.merge(default_conf, arg_conf, override_arg_conf)
-
-
-def _normalize_pipeline_name(name: str) -> str:
-    name_max_len = 82
+def normalize_pipeline_name(name: str, name_max_len: int = 82) -> str:
     name_len = len(name)
     if name_len > name_max_len:
         logging.getLogger(__name__).info(
