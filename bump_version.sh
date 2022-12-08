29fcc472ce652bd066cba31281498a01c35fefe9
Rinat Gareev <rgareev@provectus.com>
2022-12-07 20:48:09 -0800

    working on CI with poetry - 3

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 # check for uncommited changes
 if [[ `git status --porcelain` ]]; then
     git status
@@ -6,7 +7,7 @@ if [[ `git status --porcelain` ]]; then
 	exit 1
 fi
 # increment version
-new_version=`poetry version -n -s patch`
+new_version=`$POETRY_HOME/bin/poetry version -n -s patch`
 echo "Bumped to new version: $new_version"
 # commit changes
 git commit -am "[skip ci] Version updated to $new_version"
